5a945abbcf74cc244d03055c07df8aad220c6a8b
Nemanja <nemanjadragomirovic086@gmail.com>
2021-02-25 15:25:17 +0100

    Implement Aufgabe 1.1 - Erzähl mir etwas über dich

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,16 @@ public class Main {
         String VN = "Nemanja";
         String NN = "Dragomiroivc";
         int alter = 21;
-        boolean verheiratet = true;
+        boolean verheiratet = false;
         String gebDat="16.07.1999";
         System.out.println("VorName: "+VN);
         System.out.println("NachName: "+NN);
-        System.out.println("GeburtsDatum: ");
-        System.out.println("Alter: ");
+        System.out.println("GeburtsDatum: "+gebDat);
+        System.out.println("Alter: "+alter);
+
+        if (verheiratet==true)
+        {System.out.println("verheiratet: Ja");}
+        else {System.out.println("verheiratet: Nein");}
 
 
 
